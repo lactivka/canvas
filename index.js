d18a68f92f3cd55226b2a13d358f1c19fd715d5c
Stepanenko Oksana <lactivo4ka@gmail.com>
2019-10-27 20:56:09 +0200

    feat: implement drawing 4x4 matrix by button, move teg script to end of html-file

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-let arr4x4 = [
-    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
-]
+let frames = [];
 
-function draw() {
+const getFrames = () => {
+    const frames = [];
+
+    const frame = [
+        ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
+        ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+        ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+        ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
+    ]
+
+    frames.push(frame);
+    console.log(frames);
+
+    return frames;
+}
+
+function draw(frame) {
     let canvas = document.getElementById('canvas');
+    
     if (canvas.getContext) {
         let ctx = canvas.getContext('2d');
-        /*canvas.width = 4;
-        canvas.height = 4;*/
-        arr4x4.forEach((row, i) => {
+        
+       frame.forEach((row, i) => {
             row.forEach((column, j) => {
                 ctx.fillStyle = "#" + column;
                 ctx.fillRect(i, j, 4, 4);
             })
-        })
-        
+        })     
     }
 }
+
+window.onload = document.getElementById('draw').addEventListener('click', () => {
+    const frames = getFrames();
+    const frame = frames[0];
+    draw(frame);
+})
