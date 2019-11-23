d466aace4dab1244872bf15dce1b5ce61a2f1819
Stepanenko Oksana <lactivo4ka@gmail.com>
2019-11-24 01:12:56 +0200

    feat: add selectig current color from input form, change current color by clicking on prev color of one of predefined colors; add selecting tool for draw by pressing keyboard button b - for fill bucket, c - choose color, p - pencil;  save current condition of page to local storage and get it from there; change style for color circles on select color tool

--- canvas-new/index.js
+++ canvas-new/index.js
@@ -1,12 +1,38 @@
 import {data4x4} from "./data4x4.js";
 import {data32x32} from "./data32x32.js";
 
+// get data from local storage
+let dataURL = localStorage.getItem("canvasImage");
+let width = localStorage.getItem("width");
+let height = localStorage.getItem("height");
+let currentColor = localStorage.getItem("currentColor");
+let prevColor = localStorage.getItem("prevColor");
 let canvas = document.getElementById("canvas");
 
+// set select-color tools colors 
+document.querySelector(".tool__current").children[0].style.backgroundColor = currentColor;
+document.querySelector(".tool__prev").children[0].style.backgroundColor = prevColor;
+document.querySelector(".tool__red").children[0].style.backgroundColor = "#ff0000";
+document.querySelector(".tool__blue").children[0].style.backgroundColor = "#0000ff";
+
+// image canvas from local storage
+if (canvas.getContext) {
+    let ctx = canvas.getContext("2d");
+
+    canvas.width = width;
+    canvas.height = height;
+    let img = new Image;
+    img.src = dataURL;
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0);
+    }
+}
+
+// draw arrays of image on canvas
 class DrawByButton {
     handleEvent (event) {
         switch (event.target.dataset.array) {
-            case "4":
+            case "4": // 4x4 array
                 if (canvas.getContext) {
                     let ctx = canvas.getContext("2d");
                     canvas.width = 4;
@@ -19,7 +45,7 @@ class DrawByButton {
                     })
                 }
                 break;
-            case "32":
+            case "32": // 32x32 array
                 if (canvas.getContext) {
                     let ctx = canvas.getContext("2d");
                     canvas.width = 32;
@@ -32,7 +58,7 @@ class DrawByButton {
                     })
                 }
                 break;
-            case "image":
+            case "image": // image
                 if (canvas.getContext) {
                     let ctx = canvas.getContext("2d");
                     let img = new Image(); 
@@ -48,5 +74,90 @@ class DrawByButton {
     }
 }
 
+class DrawingTools {
+    activate(clicked) { // set active status for selected tool
+        document.querySelector(".active").classList.remove("active");
+        clicked.classList.add("active");
+    }
+
+    setCurrentColor(clicked) { 
+        let prevColor = document.querySelector(".tool__prev").children[0].style.backgroundColor;
+        
+        this.clicked = clicked;
+        this.changePrevTo();
+        this.changeCurrentTo(clicked, prevColor);
+    }
+
+    changePrevTo() { // change color of prev tool to current tool color
+        
+        document.querySelector(".tool__prev").children[0].style.backgroundColor = 
+        document.querySelector(".tool__current").children[0].style.backgroundColor;
+    }
+
+    changeCurrentTo(clicked, prevColor) { // change current tool color:
+
+       if (clicked.closest(".tool__prev")) {  // to prev tool color
+            document.querySelector(".tool__current").children[0].style.backgroundColor = 
+            prevColor;
+        }
+
+        if (clicked.closest(".tool__red")) { // to red
+            document.querySelector(".tool__current").children[0].style.backgroundColor = 
+            document.querySelector(".tool__red").children[0].style.backgroundColor;   
+        }
+          
+        if (clicked.closest(".tool__blue")) { // to blue
+           document.querySelector(".tool__current").children[0].style.backgroundColor = 
+           document.querySelector(".tool__blue").children[0].style.backgroundColor;   
+        } 
+         
+    }
+
+    changeCurrentFromInput() { // change current color to color selected in input
+        
+           document.querySelector(".tool__current").children[0].style.backgroundColor = 
+           document.querySelector(".color-input").value;
+
+           document.querySelector(".color-input").value = "#eeebeb";
+    }
+}
+
 let draw = new DrawByButton();
+let tool = new DrawingTools();
+
 window.onload = document.querySelector(".draw").addEventListener("click", draw);
+
+window.onload = document.querySelector(".tools-for-draw").addEventListener("click", (event) => {
+    tool.activate(event.target);
+} );
+
+window.onload = document.querySelector(".color-input").onchange =  () => {
+    tool.changeCurrentFromInput();
+}
+
+window.onload = document.querySelector(".select-color").addEventListener("click", (event) => {
+    tool.setCurrentColor(event.target);     
+});
+// select tool for draw by pressing keyboard key 
+window.onload = document.addEventListener("keydown", (event) => {
+
+    switch(event.code) {
+        case "KeyB": // select fill bucket
+            tool.activate(document.querySelector(".tool__bucket"));
+            break;
+        case "KeyC": // select choose color
+            tool.activate(document.querySelector(".tool__choose"));
+            break;
+        case "KeyP": // select pencil
+            tool.activate(document.querySelector(".tool__pencil"));
+            break;
+    }
+})
+// save current condition to local storage
+window.onbeforeunload = () => {
+    localStorage.setItem("canvasImage", canvas.toDataURL());
+    localStorage.setItem("width", canvas.width);
+    localStorage.setItem("height", canvas.height);
+    localStorage.setItem("currentColor", document.querySelector(".tool__current").children[0].style.backgroundColor);
+    localStorage.setItem("prevColor",  document.querySelector(".tool__prev").children[0].style.backgroundColor);
+}
